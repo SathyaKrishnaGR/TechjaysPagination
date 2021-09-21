5bfad332bae85f7f4707ca7423ee79574ba4e10d
SathyaKrishna <sathya.gr@iderize.in>
2021-09-21 18:19:57 +0530

    FwtchData access changed

--- Sources/TechjaysPagination/PaginatedViews/PaginatedCollectionView.swift
+++ Sources/TechjaysPagination/PaginatedViews/PaginatedCollectionView.swift
@@ -41,7 +41,7 @@ class PaginatedCollectionView: UICollectionView {
     
     /// Fetch data from server from the given offset with default fetch limit.
     /// - Parameter from: offset of the api data
-    func fetchData(from offset: Int = 0) {
+    public func fetchData(from offset: Int = 0) {
         isLoading = true
         paginationManager.paginate(to: offset) { (hasNext) in
             self.isLoading = false
