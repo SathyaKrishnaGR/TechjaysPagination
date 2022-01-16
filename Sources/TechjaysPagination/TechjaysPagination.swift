260d647f1b368feb2bc080242f6efe4a21656724
SathyaKrishnaGR <69797138+SathyaKrishnaGR@users.noreply.github.com>
2022-01-16 13:49:07 +0530

    collectionView added

--- Sources/TechjaysPagination/TechjaysPagination.swift
+++ Sources/TechjaysPagination/TechjaysPagination.swift
@@ -1,6 +1,7 @@
 public struct TechjaysPagination {
     var paginatedTableView: PaginatedTableView!
     var paginatedCollectionView: PaginatedCollectionView!
+    
     public static let shared = TechjaysPagination()
     public func setupPagination(baseUrl: String, version: String, defaultOffset: String) {
         URLFactory.shared.baseUrl = baseUrl
